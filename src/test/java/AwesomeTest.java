746e75837ee177708f1ea41f3b5f3762093e5237
hysnn <hsynn03@gmail.com>
2020-10-08 06:52:12 +0300

    notes in AwesomeTest folder

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,8 @@
+public class AwesomeTest {
+
+    // red means its untracked
+    // green means its under staging area
+    // grey in .idea folder means ignored
+    // when color is normal, it means its commited changes
+
+}
